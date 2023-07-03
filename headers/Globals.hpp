ef099fef125b1fc804b22ea5561741bd4c52f0c3
KrashM <hristo_mesi1990@abv.bg>
2023-07-03 23:59:05 +0300

    Added font support

--- headers/Globals.hpp
+++ headers/Globals.hpp
@@ -2,6 +2,7 @@
 
 #include <cstdint>
 #include <cstddef>
+#include <iostream>
 
 namespace SnakeGame{
 
@@ -14,9 +15,12 @@ namespace SnakeGame{
 
     std::size_t const GRID_WIDTH = 32;
     std::size_t const GRID_HEIGHT = 32;
-    std::size_t const WINDOW_WIDTH = 640;
-    std::size_t const WINDOW_HEIGHT = 640;
+    std::size_t const WINDOW_WIDTH = 800;
+    std::size_t const WINDOW_HEIGHT = 800;
     std::size_t const FRAMES_PER_SECOND = 60;
     std::size_t const MS_PER_FRAME = 1000 / FRAMES_PER_SECOND;
+
+    std::size_t const FONT_SIZE = 24;
+    char const * const FONT_NAME = "resources/fonts/Belanosima-Regular.ttf";
 
 }
